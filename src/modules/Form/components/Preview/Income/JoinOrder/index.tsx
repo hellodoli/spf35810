c55b620e5566983fe12ab045c9a72de23cef0c70
Phat <nmphat0912@gmail.com>
2025-02-17 16:02:07 +0700

    Chore: change default isDetail preview JoinOrder

--- src/modules/Form/components/Preview/Income/JoinOrder/index.tsx
+++ src/modules/Form/components/Preview/Income/JoinOrder/index.tsx
@@ -1,6 +1,7 @@
-import React, { memo, useMemo, useState } from 'react'
+import React, { memo, useCallback, useMemo, useState } from 'react'
 import { useSelector } from 'react-redux'
 
+import { ExpandBtn } from 'components/Buttons'
 import { joinsSelector } from 'modules/Form/selectors'
 import { getPriceJoinOrder } from 'utils/price'
 import { getFormat } from 'utils/price'
@@ -10,7 +11,7 @@ const f = getFormat()
 
 const JoinOrderComp = () => {
   const joins = useSelector(joinsSelector)
-  const [isDetail, setIsDetail] = useState(true)
+  const [isDetail, setIsDetail] = useState(false)
   const priceJO = useMemo(() => getPriceJoinOrder({ joins }), [joins])
 
   const renderDetailJoinOrderPrice = () => {
@@ -18,18 +19,17 @@ const JoinOrderComp = () => {
     return <JoinOrderList joins={joins} />
   }
 
+  const toggleDetail = useCallback(() => {
+    setIsDetail((open) => !open)
+  }, [])
+
   return (
     <li>
       <span>Thu nhập đơn ghép:</span>
       <strong className="ml-1">{f(priceJO)}</strong>
       {/* Chi Tiết Thu Nhập đơn ghép */}
       {priceJO > 0 && (
-        <span
-          className="ml-1 link cursor-pointer select-none"
-          onClick={() => setIsDetail((open) => !open)}
-        >
-          {`(${isDetail ? 'Ẩn' : 'Chi tiết'})`}
-        </span>
+        <ExpandBtn isDetail={isDetail} toggleDetail={toggleDetail} />
       )}
       {renderDetailJoinOrderPrice()}
     </li>
